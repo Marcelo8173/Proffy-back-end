30773114cf04666e961d162b4fc28766c9b79f77
Marcelo8173 <marceloandrebio@gmail.com>
2020-08-04 22:56:28 -0400

    list all connections

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,17 @@ import express, { request, response } from 'express';
 import db from './database/connections';
 import convertTime from './utils/convert';
 import ClassesController from './controllers/classesController';
+import ConnectinsController from './controllers/connectionsControllers';
 
 const routes = express.Router();
 
 const classesController = new ClassesController();
+const connectionsContrller = new ConnectinsController();
 
 routes.post('/classes',classesController.create);
 routes.get('/classes',classesController.index);
+routes.post('/connections', connectionsContrller.create);
+routes.get('/connections', connectionsContrller.index);
 
 
 export default routes;
